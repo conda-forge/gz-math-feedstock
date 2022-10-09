980804d053313e75d93ab316cf5178a4f41411bc
Silvio Traversaro <silvio@traversaro.it>
2022-10-09 15:06:35 +0200

    Update build_py.sh

--- recipe/build_py.sh
+++ recipe/build_py.sh
@@ -11,13 +11,16 @@ rm -rf build
 mkdir build
 cd build
 
+echo "Setting Python3_INCLUDE_DIRS to "$PREFIX/include/`ls $PREFIX/include | grep python`
+
+
 cmake ${CMAKE_ARGS} -GNinja .. \
     -DCMAKE_BUILD_TYPE=Release \
     -DBUILD_SHARED_LIBS:BOOL=ON \
     -DUSE_SYSTEM_PATHS_FOR_PYTHON_INSTALLATION:BOOL=ON \
     -DPython3_EXECUTABLE:PATH=$PYTHON \
     -DPYTHON_EXECUTABLE:PATH=$PYTHON \
-    -DPython3_INCLUDE_DIRS:PATH=$PREFIX/include`ls $PREFIX/include | grep python`
+    -DPython3_INCLUDE_DIRS:PATH=$PREFIX/include/`ls $PREFIX/include | grep python`
 
 cmake --build . --config Release
 cmake --build . --config Release --target install
